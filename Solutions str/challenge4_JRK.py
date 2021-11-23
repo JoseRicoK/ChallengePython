de9e2544c26b648f07f5ea23844e5703d5a936f3
JoseRicoK <josemariark@gmail.com>
2021-11-23 23:54:43 +0100

    añadidos

--- Solutions str/challenge4_JRK.py
+++ Solutions str/challenge4_JRK.py
@@ -2,8 +2,8 @@ import math
 
 print('Welcome to the Right Triangle Solver App')
 
-lado1 = float(input('What is the first leg of the triangle: '))
+lado1 = float(input('\nWhat is the first leg of the triangle: '))
 lado2 = float(input('What is the second leg of the triangle:'))
 hipotenusa = math.sqrt((lado1**2) + (lado2**2))
-print(f'For a triangle with legs of {lado1} and {lado2} the hypotenuse is {round(hipotenusa,3)}.')
+print(f'\nFor a triangle with legs of {lado1} and {lado2} the hypotenuse is {round(hipotenusa,3)}.')
 print(f'For a triangle with legs of {lado1} and {lado2} the area is {round((lado1*lado2)/2,3)}.')
